237ee7d72243fd9f4602a75f2266d4837ff30322
DanielRumay <165932669+DanielRumay@users.noreply.github.com>
2024-09-01 19:27:37 +0000

    test

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php require "./inc/session.php";
+?>
+!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+<link rel="stylesheet" href="bootstrap-5.0.2-dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="/css/fondo.css">
 
-include "./inc/head.php";
-
+</head>
+    <body>
+<?php
 if (!isset($_GET['vista']) || $_GET['vista'] == "") {
     $_GET['vista'] = "login";
 }
@@ -21,5 +31,8 @@ if (is_file("./vistas/" . $_GET['vista'] . ".php") && $_GET['vista'] != "login" 
         include "./vistas/404.php";
     }
 }
+?>
+<script src="bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+</body>
 
-include "./inc/footer.php";
+</html>
